ad993d03b38ee3964176a559703ee6497d348d14
jarrodyellets <jarrod524@gmail.com>
2017-11-20 19:52:30 +0100

    first commit

---
+++ sumAllPrimes.js
@@ -0,0 +1,26 @@
+// Sum all the prime numbers up to and including the provided number.
+
+// A prime number is defined as a number greater than one and having only two divisors, one and itself. 
+// For example, 2 is a prime number because it's only divisible by one and two.
+
+// The provided number may not be a prime.
+
+
+function sumPrimes(num) {
+  var result = 0;
+  
+  for (var i = 2; i <= num; i++){
+    var isPrime = true;
+    for (var j = 2; j < i; j++){
+      if (i % j === 0){
+        isPrime = false;
+      }
+    }
+    if (isPrime){
+        result += i;
+        
+    }
+  }
+  
+  return result;
+}
