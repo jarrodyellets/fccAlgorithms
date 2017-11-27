d6160cd1d3b633e3348b4ee7f572dbbc4299894f
jarrodyellets <jarrod524@gmail.com>
2017-11-27 23:34:11 +0100

    fixed comments

--- validatePhoneNumber.js
+++ validatePhoneNumber.js
@@ -1,3 +1,4 @@
+
 // Your job is to validate or reject the US phone number based on any combination of the formats provided 
 // above. The area code is required. If the country code is provided, you must confirm that the country 
 // code is 1. Return true if the string is a valid US phone number; otherwise return false.
